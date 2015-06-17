f4cf86518a1e260e0bdacbe488dc13f941193033
Puru Dahal <pdahal@me.com>
2015-06-16 20:08:45 -0700

    Require dependencies on gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+// require dependencies
+var gulp          = require('gulp'),
+    browserify    = require('browserify'),
+    reactify      = require('reactify'),
+    source        = require('vinyl-source-stream');
